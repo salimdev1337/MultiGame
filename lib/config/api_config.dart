f4e306e31c0241d55f4d2be443b9455508166ae7
salim mtiri <salimmtiri17@gmail.com>
2026-01-18 15:15:35 +0100

    test api

---
+++ lib/config/api_config.dart
@@ -0,0 +1,6 @@
+class ApiConfig {
+  static const String unsplashBaseUrl = 'https://api.unsplash.com';
+  static const String unsplashAccessKey = String.fromEnvironment(
+    'UNSPLASH_ACCESS_KEY',
+  );
+}
