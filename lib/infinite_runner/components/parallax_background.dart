c73963be6f49443b4f19d43da06d6c96976f818d
salim mtiri <salimmtiri17@gmail.com>
2026-01-21 17:55:07 +0100

    testing github actions assets load

--- lib/infinite_runner/components/parallax_background.dart
+++ lib/infinite_runner/components/parallax_background.dart
@@ -1,7 +1,6 @@
 import 'package:flame/components.dart';
 import 'package:flame/parallax.dart';
 import 'package:flutter/painting.dart';
-import 'package:flutter/services.dart';
 
 class ParallaxBackground extends ParallaxComponent {
   ParallaxBackground({required Vector2 size, required double scrollSpeed})
@@ -14,32 +13,21 @@ class ParallaxBackground extends ParallaxComponent {
   Future<void> onLoad() async {
     await super.onLoad();
 
-    // Manually load images from assets/background/ and register with Flame
-    await _loadAndRegisterImage('sky.png', 'background/PNG/Flat/sky.png');
-    await _loadAndRegisterImage(
-      'mountain1.png',
-      'background/PNG/Flat/mountain1.png',
-    );
-    await _loadAndRegisterImage('hills1.png', 'background/PNG/Flat/hills1.png');
-    await _loadAndRegisterImage(
-      'clouds1.png',
-      'background/PNG/Flat/clouds1.png',
-    );
-
-    // Load parallax with registered images
+    // Use Flame's standard asset loading (works on web)
+    // Images should be in assets/images/background/PNG/Flat/
     parallax = await game.loadParallax(
       [
         // Layer 1: Sky - Static background (no scrolling)
-        ParallaxImageData('sky.png'),
+        ParallaxImageData('background/PNG/Flat/sky.png'),
 
         // Layer 2: Mountains - Slow scroll (0.3x speed)
-        ParallaxImageData('mountain1.png'),
+        ParallaxImageData('background/PNG/Flat/mountain1.png'),
 
         // Layer 3: Hills - Medium scroll (0.6x speed)
-        ParallaxImageData('hills1.png'),
+        ParallaxImageData('background/PNG/Flat/hills1.png'),
 
         // Layer 4: Clouds - Fast scroll (1.0x speed)
-        ParallaxImageData('clouds1.png'),
+        ParallaxImageData('background/PNG/Flat/clouds1.png'),
       ],
       baseVelocity: Vector2(_scrollSpeed, 0),
       velocityMultiplierDelta: Vector2(1.4, 0),
@@ -47,13 +35,6 @@ class ParallaxBackground extends ParallaxComponent {
       repeat: ImageRepeat.repeatX,
       alignment: Alignment.bottomCenter,
     );
-  }
-
-  Future<void> _loadAndRegisterImage(String key, String assetPath) async {
-    final data = await rootBundle.load(assetPath);
-    final bytes = data.buffer.asUint8List();
-    final image = await decodeImageFromList(bytes);
-    game.images.add(key, image);
   }
 
   /// Update scroll speed (called when game speed changes)
